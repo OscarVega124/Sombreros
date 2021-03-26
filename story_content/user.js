b72146e9078fab2cb710258aea25e4e87a575fae
OscarVega124 <71149590+OscarVega124@users.noreply.github.com>
2021-03-25 22:03:32 -0600

    Add files via upload

--- story_content/user.js
+++ story_content/user.js
@@ -2,14 +2,23 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6So7nlRnOpa":
+      case "6kIkEKyi7s6":
         Script1();
         break;
-      case "6X4rLqqlNic":
+      case "6Js8gZJcOoL":
         Script2();
         break;
-      case "6QCj1YR7zMZ":
+      case "6gdZ1bVqNkm":
         Script3();
+        break;
+      case "6BWJiBKgYo3":
+        Script4();
+        break;
+      case "5mMbsfSNDop":
+        Script5();
+        break;
+      case "5b1G0w84hip":
+        Script6();
         break;
   }
 }
@@ -40,7 +49,7 @@ storyline =
  "NOMBRE" : player.GetVar("Nombre"),
  "NIVEL ACADEMICO" : player.GetVar("NivelEstudios"),
   "INSTITUCION" : player.GetVar("Institucion"),
-  "SEMESTRE" : player.GetVar("Semestre"),
+  "SEMESTRE" : player.GetVar("Semestres"),
   "DIAPOSITIVAS VISUALISADAS" : player.GetVar("VIS"),
   "TOTAL DE DIAPOSITIVAS" : player.GetVar("TOT"),
   "CANTIDAD DE INTENTOS" : player.GetVar("intentos"),
@@ -50,6 +59,63 @@ storyline =
 }
 
 function Script3()
+{
+  //DELAY SO JQUERY LIBRARY IS LOADED
+setTimeout(function (){
+ 
+//Export to Google
+$.ajax({
+url: WEB_APP_URL,
+type: "POST",
+data : storyline,
+success: function(data)
+{
+console.log(data);
+},
+error: function(err) {
+console.log('Error:', err);
+}
+});
+return false;
+}, 1000);
+}
+
+function Script4()
+{
+  var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+head.appendChild(script)
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+ 
+//PLACE YOUR WEB APP URL
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbx_SYb5VUiurfJoi858TA7GQRp3Qj-sRyfCTdyEQdGYa7iFGJ9y/exec";
+ 
+// STORE ARTICULATE STORYLINE VARIABLES
+// "Columnname_Google_Spreadsheet" : player.GetVar("Name_Storyline_Variable")
+// ATTENTION: Use a comma if you use multiple Storyline variables
+storyline =
+{
+ "FECHA" : new Date().toJSON().slice(0,10), //STORE DATE
+ "MATRICULA" : player.GetVar("Matricula"),
+ "NOMBRE" : player.GetVar("Nombre"),
+ "NIVEL ACADEMICO" : player.GetVar("NivelEstudios"),
+  "INSTITUCION" : player.GetVar("Institucion"),
+  "SEMESTRE" : player.GetVar("Semestres"),
+  "DIAPOSITIVAS VISUALISADAS" : player.GetVar("VIS"),
+  "TOTAL DE DIAPOSITIVAS" : player.GetVar("TOT"),
+  "CANTIDAD DE INTENTOS" : player.GetVar("intentos"),
+  "INTENTOS <70" : player.GetVar("noaprobado"),
+  "INTENTOS > 69.99" : player.GetVar("aprobado")
+}
+}
+
+function Script6()
 {
   //DELAY SO JQUERY LIBRARY IS LOADED
 setTimeout(function (){
